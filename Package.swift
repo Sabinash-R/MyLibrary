869dbf0f6f908a1cd66887b1ac614256aeea336d
ravichandran raju <fitliferavi@ravichandrans-MacBook-Pro.local>
2026-07-04 10:38:29 +0530

    Fix Package.swift with real URL and checksum

--- Package.swift
+++ Package.swift
@@ -10,8 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MyLibrary",
-            url: "https://github.com/Sabinash-R/MyLibrary/releases/download/1.0.0/MyLibrary.xcframework.zip
-MyLibrary.xcframework.zip",
+            url: "https://github.com/Sabinash-R/MyLibrary/releases/download/1.0.0/MyLibrary.xcframework.zip",
             checksum: "4f1dab53e795fb0b9bcd378fc8d8fa593bda4dada57fc9b1fc8dc56bc131de2f"
         )
     ]
