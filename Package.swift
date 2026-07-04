a76009ccae2f2623e50f36613f646412f2a9ea82
ravichandran raju <fitliferavi@Mac.lan>
2026-07-04 20:24:52 +0530

    Fix checksum mismatch for 1.1.0

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MyLibrary",
-            url: "https://github.com/Sabinash-R/MyLibrary/releases/download/1.0.0/MyLibrary.xcframework.zip",
+            url: "https://github.com/Sabinash-R/MyLibrary/releases/download/1.1.0/MyLibrary.xcframework.zip",
             checksum: "13db531ebd9ac19e9ff5011df5cce84960bc85fd3406db5b1bb7697622d04da5"
         )
     ]
